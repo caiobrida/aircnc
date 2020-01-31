a9c6d83435dc10ded6df9b54a68f64ded653893b
ayax <caiobrida1@gmail.com>
2020-01-31 06:09:52 -0300

    up aircnc

---
+++ backend/src/models/Spot.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const spotSchema = new mongoose.Schema({
+    thumbnail: String,
+    company: String,
+    price: Number,
+    techs: [String],
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, {
+    toJSON: {
+        virtuals: true,
+    },
+});
+
+spotSchema.virtual('thumbnail_url').get(function() {
+    return `http://localhost:3333/files/${this.thumbnail}`
+})
+
+module.exports = mongoose.model('Spot', spotSchema);
